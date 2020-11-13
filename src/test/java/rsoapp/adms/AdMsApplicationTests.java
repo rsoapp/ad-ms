73064d530aa428f7818431ae1d19199a30635f71
Maj Šavli <majsavli@gmail.com>
2020-11-13 12:31:02 +0100

    init project

---
+++ src/test/java/rsoapp/adms/AdMsApplicationTests.java
@@ -0,0 +1,13 @@
+package rsoapp.adms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdMsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
